3e5de2efe71b890a679ab49a03c77612473d6257
Marcel R <marcelrieger@me.com>
2018-01-24 23:10:25 +0100

    Use spaces in bash scripts.

--- devenv.sh
+++ devenv.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
 
 action() {
-	local base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
-	local parent="$( dirname "$base" )"
+    local base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
+    local parent="$( dirname "$base" )"
 
-	_addpy() {
-		[ ! -z "$1" ] && export PYTHONPATH="$1:$PYTHONPATH"
-	}
+    _addpy() {
+        [ ! -z "$1" ] && export PYTHONPATH="$1:$PYTHONPATH"
+    }
 
-	_addbin() {
-		[ ! -z "$1" ] && export PATH="$1:$PATH"
-	}
+    _addbin() {
+        [ ! -z "$1" ] && export PATH="$1:$PATH"
+    }
 
-	# source dev software
-	export LAW_DEV_SOFTWARE="$base/devsoftware"
-	if [ -d "$LAW_DEV_SOFTWARE" ]; then
-		# luigi
-		_addpy "$LAW_DEV_SOFTWARE/luigi"
-		_addbin "$LAW_DEV_SOFTWARE/luigi/bin"
+    # source dev software
+    export LAW_DEV_SOFTWARE="$base/devsoftware"
+    if [ -d "$LAW_DEV_SOFTWARE" ]; then
+        # luigi
+        _addpy "$LAW_DEV_SOFTWARE/luigi"
+        _addbin "$LAW_DEV_SOFTWARE/luigi/bin"
 
-		# six
-		_addpy "$LAW_DEV_SOFTWARE/six"
+        # six
+        _addpy "$LAW_DEV_SOFTWARE/six"
 
-		# scinum
-		_addpy "$LAW_DEV_SOFTWARE/scinum"
+        # scinum
+        _addpy "$LAW_DEV_SOFTWARE/scinum"
 
-		# order
-		_addpy "$LAW_DEV_SOFTWARE/order"
+        # order
+        _addpy "$LAW_DEV_SOFTWARE/order"
 
-		# law
-		_addpy "$LAW_DEV_SOFTWARE/law"
-		_addbin "$LAW_DEV_SOFTWARE/law/bin"
-		source "$( law completion )"
-	fi
+        # law
+        _addpy "$LAW_DEV_SOFTWARE/law"
+        _addbin "$LAW_DEV_SOFTWARE/law/bin"
+        source "$( law completion )"
+    fi
 
-	# add _this_ repo
-	_addpy "$base"
+    # add _this_ repo
+    _addpy "$base"
 }
 action "$@"
--- devinstall.sh
+++ devinstall.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
 action() {
-	local base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
+    local base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
-	local sw_base="$base/devsoftware"
-	echo "installing development software in $sw_base"
-	mkdir -p "$sw_base"
+    local sw_base="$base/devsoftware"
+    echo "installing development software in $sw_base"
+    mkdir -p "$sw_base"
 
-	_install_git() {
-		local name="$1"
-		local url="$2"
-		local tag="$3"
-		if [ ! -d "$sw_base/$name" ]; then
-			( cd "$sw_base" && git clone "$url" )
-			if [ "$?" != "0" ]; then
-				2>&1 echo "$name checkout failed"
-				return "1"
-			fi
-			if [ ! -z "$tag" ]; then
-				( cd "$sw_base/$name" && git checkout "tags/$tag" )
-			fi
-		else
-			echo "skipping $name, already exists"
-		fi
-	}
+    _install_git() {
+        local name="$1"
+        local url="$2"
+        local tag="$3"
+        if [ ! -d "$sw_base/$name" ]; then
+            ( cd "$sw_base" && git clone "$url" )
+            if [ "$?" != "0" ]; then
+                2>&1 echo "$name checkout failed"
+                return "1"
+            fi
+            if [ ! -z "$tag" ]; then
+                ( cd "$sw_base/$name" && git checkout "tags/$tag" )
+            fi
+        else
+            echo "skipping $name, already exists"
+        fi
+    }
 
-	# luigi
-	_install_git "luigi" "https://github.com/spotify/luigi.git" "2.7.2" || return "$?"
+    # luigi
+    _install_git "luigi" "https://github.com/spotify/luigi.git" "2.7.2" || return "$?"
 
-	# six
-	_install_git "six" "https://github.com/benjaminp/six.git" "1.11.0" || return "$?"
+    # six
+    _install_git "six" "https://github.com/benjaminp/six.git" "1.11.0" || return "$?"
 
-	# scinum
-	_install_git "scinum" "https://github.com/riga/scinum.git" || return "$?"
+    # scinum
+    _install_git "scinum" "https://github.com/riga/scinum.git" || return "$?"
 
-	# order
-	_install_git "order" "https://github.com/riga/order.git" || return "$?"
+    # order
+    _install_git "order" "https://github.com/riga/order.git" || return "$?"
 
-	# law
-	_install_git "law" "https://github.com/riga/law.git" || return "$?"
+    # law
+    _install_git "law" "https://github.com/riga/law.git" || return "$?"
 }
 action "$@"
