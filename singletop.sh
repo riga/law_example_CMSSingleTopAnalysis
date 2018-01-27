bd36cce3dad26d3dc91f01ab74e9953ff4c096fd
Marcel R <marcelrieger@me.com>
2018-01-27 15:09:07 +0100

    Configure LAW_HOME in singletop setup.

--- singletop.sh
+++ singletop.sh
@@ -3,10 +3,13 @@
 action() {
     local base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 
+    # software variables
+    export LAW_HOME="$ANALYSIS_BASE/.law"
+    export LAW_CONFIG_FILE="$ANALYSIS_BASE/law.cfg"
+    export LUIGI_CONFIG_PATH="$ANALYSIS_BASE/luigi.cfg"
+
     # analysis variables
     export ANALYSIS_BASE="$base"
     export ANALYSIS_LOCAL_STORE="$ANALYSIS_BASE/data"
-    export LAW_CONFIG_FILE="$ANALYSIS_BASE/law.cfg"
-    export LUIGI_CONFIG_PATH="$ANALYSIS_BASE/luigi.cfg"
 }
 action
